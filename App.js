eb00a82086a9e1987358735c46cbf0551d4d1e57
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2025-12-23 19:50:06 +0530

    Basic React using CDN link and a js file

---
+++ App.js
@@ -0,0 +1,17 @@
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const parent = React.createElement("div",{id:'parent'}, [
+
+    React.createElement("div",{id:'child'},[
+        React.createElement("h1",{}, "I am H1 Tag"),
+        React.createElement("h2",{}, "I am H2 Tag")
+    ]),
+    React.createElement("div",{id:'child2'},[
+        React.createElement("h1",{}, "I am H1 Tag"),
+        React.createElement("h2",{}, "I am H2 Tag")
+    ]
+)
+]
+)
+
+root.render(parent)
