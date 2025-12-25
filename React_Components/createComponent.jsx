2154a7364398f086378aebb09e8e2d0e3c4790a4
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2025-12-25 22:01:17 +0530

    created A basic component

---
+++ React_Components/createComponent.jsx
@@ -0,0 +1,12 @@
+function Button(){
+    return (
+        <div>
+            <button>MY BUTTON</button>
+        </div>
+    )
+}
+// - its a component for button
+// - component names must start with capital
+// - its a reusable button
+// - A component return JSX
+export default Button // A component should be exported
