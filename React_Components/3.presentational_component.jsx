3a5918cb3083230bd8cb68259fc8861fe750e2ff
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2025-12-26 09:00:08 +0530

    presenattion componnets

---
+++ React_Components/3.presentational_component.jsx
@@ -0,0 +1,11 @@
+function Label({label}){
+    return <label>{label}</label>
+}
+
+// componnet which only render UI , no state no side effects
+
+// Pure functions
+
+// Fully driven by props
+
+// Easy to reuse and test
